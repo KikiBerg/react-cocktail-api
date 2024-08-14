edd24d4bbb66c75bc2edefbcb159100db153512d
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-08-14 14:51:02 +0200

    Finish SearchResults component

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,11 +1,44 @@
-import { ReactElement } from "react";
-import { ISearchResultsProps } from "../interfaces";
+import { ReactElement } from 'react';
+import { ISearchResultsProps } from '../interfaces';
 import '../css/SearchPage.css';
 
-export function SearchResults ({ results, currentPage, onPageChange, onCocktailSelect }: ISearchResultsProps) : ReactElement {
-    const itemsPerPage = 8;
-    const totalPages = Math.ceil(results.length / itemsPerPage);
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const currentResults = results.slice(startIndex, endIndex);
+export function SearchResults({
+  results,
+  currentPage,
+  onPageChange,
+  onCocktailSelect,
+}: ISearchResultsProps): ReactElement {
+  const itemsPerPage = 8;
+  const totalPages = Math.ceil(results.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentResults = results.slice(startIndex, endIndex);
+
+  return (
+    <div className="search-results">
+      <ul>
+        {currentResults.map((cocktail) => (
+          <li
+            key={cocktail.idDrink}
+            onClick={() => onCocktailSelect(cocktail.idDrink)}
+          >
+            {cocktail.strDrink}
+          </li>
+        ))}
+      </ul>
+      {totalPages > 1 && (
+        <div className="pagination">
+          {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+            <button
+              key={page}
+              onClick={() => onPageChange(page)}
+              className={currentPage === page ? 'active' : ''}
+            >
+              {page}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
