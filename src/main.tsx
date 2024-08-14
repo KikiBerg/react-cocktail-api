1c5746a405d3f49ad9d26507aa826824aa0df4ae
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-08-14 11:23:12 +0200

    Define initial main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./components";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+//import { App } from "./components";
 
-import "./css/index.css";
+//import "./css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
