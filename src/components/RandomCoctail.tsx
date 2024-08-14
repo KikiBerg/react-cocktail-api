7dea06e79a21302b2234681b2bee607424790097
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-08-14 16:26:56 +0200

    Update the 'See More' button to navigate to the Cocktail Info Page

--- src/components/RandomCoctail.tsx
+++ src/components/RandomCoctail.tsx
@@ -1,4 +1,5 @@
 import { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
 import { IRandomCocktailProps } from '../interfaces';
 import '../css/LandingPage.css';
 
@@ -13,7 +14,9 @@ export function RandomCocktail({
           <h2>{cocktail.strDrink}</h2>
           <img src={cocktail.strDrinkThumb} alt={cocktail.strDrink} />
           <button onClick={onGetRandom}>Get Another Random Cocktail</button>
-          <button>See More</button>
+          <Link to={`/cocktail/${cocktail.idDrink}`}>
+            <button>See More</button>
+          </Link>
         </div>
       ) : (
         <button onClick={onGetRandom}>Get Random Cocktail</button>
