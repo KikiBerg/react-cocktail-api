58def2f2d60333f4b52ef7d724abe8b04a36261c
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-08-14 15:11:35 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ReactElement } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 //import "./App.css";
 
 export function App(): ReactElement {
   return (
     <div className="App">
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/search">Search</Link>
+      </nav>
       <Outlet />
     </div>
   );
