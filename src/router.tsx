cf73e2f24c44e559173e7d6148fc80b83a98d7f3
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-08-14 15:09:42 +0200

    Add index.ts in pages folder & update router

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom"; 
-import LandingPage from './pages/LandingPage';
+import { LandingPage, SearchPage } from './pages';
+
 
 export const router = createBrowserRouter ([
     {
         path: '/',
         element: <LandingPage/>,
+    },
+    {
+        path: '/search',
+        element: <SearchPage/>,
     },
 ]);
