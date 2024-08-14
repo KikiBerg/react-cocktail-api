0315170874b34d60f04e64ef5ec804d19f5d0ef0
KikiBerg <153670000+KikiBerg@users.noreply.github.com>
2024-08-14 15:02:37 +0200

    Add handleCoctailSelect with navigate

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -21,6 +21,10 @@ export function SearchPage() : ReactElement {
             setSearchResults([]);
         }
     };
+    
+    const handleCocktailSelect = (id: string) => {
+        navigate(`/cocktail/${id}`);
+    };
 
     
 }
